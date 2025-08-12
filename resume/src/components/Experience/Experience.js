2b4cda33771a2c7ea62ef2e458adf16692399b97
Tyler Chenoweth <tchen358@gmail.com>
2025-08-12 20:23:09 +0000

    idk

--- resume/src/components/Experience/Experience.js
+++ resume/src/components/Experience/Experience.js
@@ -1,37 +1,28 @@
-import styles from "./Experience.module.css";
+import styles from "./experience.module.css";
 
 const Experience = ({ json_payload }) => {
   const monthAbbr = {
-    1: "Jan",
-    2: "Feb",
-    3: "Mar",
-    4: "Apr",
-    5: "May",
-    6: "Jun",
-    7: "Jul",
-    8: "Aug",
-    9: "Sep",
-    10: "Oct",
-    11: "Nov",
-    12: "Dec",
+    1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun",
+    7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec",
   };
 
   return (
     <>
-      <div className={styles.row}>
+      {/* header row: company + dates */}
+      <div className={`${styles.row} ${styles.rowHeader}`}>
         <h2 className={`${styles.rowLeft} ${styles.rowElement}`}>
           {json_payload.company_name}
         </h2>
         <p className={`${styles.rowRight} ${styles.rowElement}`}>
-          {monthAbbr[json_payload.start_date.month]}&nbsp;
-          {json_payload.start_date.year}
-          &nbsp;-&nbsp;
+          {monthAbbr[json_payload.start_date.month]} {json_payload.start_date.year}
+          {" \u2013 "}
           {json_payload.end_date.year === 0
             ? "Present"
             : `${monthAbbr[json_payload.end_date.month]} ${json_payload.end_date.year}`}
         </p>
       </div>
 
+      {/* sub row: title + location */}
       <div className={styles.row}>
         <p className={`${styles.rowLeft} ${styles.rowElement}`}>
           {json_payload.position}
@@ -42,8 +33,8 @@ const Experience = ({ json_payload }) => {
       </div>
 
       <ul className={styles.bullets}>
-        {json_payload.job_bullets.map((bullet, index) => (
-          <li className={styles.bullet} key={index}>
+        {json_payload.job_bullets.map((bullet, i) => (
+          <li className={styles.bullet} key={i}>
             {bullet}
           </li>
         ))}
